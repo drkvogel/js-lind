3cc81704b5d9fad1d0d87e638e91856e2283425b
Chris Bird <chrisjbird@gmail.com>
2017-02-07 07:25:33 +0000

    more

--- yo/app/scripts/main.js
+++ yo/app/scripts/main.js
@@ -1,6 +1,6 @@
 console.log('Ready...');
 
-var stage, nrand;
+var stage = "start", nrand;
 
 //  subroutine simplerand2(seed,iform,nrand,propx,nranda,nrandb, 
 //      fact1_1a,fact1_1b,fact1_2a,fact1_2b,fact2_1a,fact2_1b,      factor 1, strata 1/2, treatment a/b
@@ -53,7 +53,7 @@ var factors = { // each strata for each factor has a tuple for a count of patien
 };
 
 var nranda, nrandb;
-var errorNrandRec   = "<p><font color=\"red\">*** Number of patients must be between 0 and 10000 ***</font></p>\n";
+var errorNrandRec = "<p><font color=\"red\">*** Number of patients must be between 0 and 10000 ***</font></p>\n";
 
 // gets NRAND random numbers between 0.0 & 1.0 for each of the 4 factor groups FACT1, FACT2, FACT3 & FACTX,
 // to determine the distributions between 2 theoretical treatment groups, treatment A & treatment B
@@ -64,16 +64,28 @@ var errorNrandRec   = "<p><font color=\"red\">*** Number of patients must be bet
 // e.g. FACT3(2,1) is the number of patients for factor 2 (age), category 2 (15-34), on treatment A
 // possible gotchas: rand seed, 16-bit floating point rounding errors
 
+// handle enter key on input
+$(function () {
+    $("#nrandrec").keypress(function (e) {
+        if (e.keyCode == 13) { // enter
+            simplerand();
+        }
+    });
+});
+
 function simplerand() { // Takes in nrand, iform & propx?
-    var enteredNumber = parseInt($('#nrandrec').val(), 10);
-    if (isNaN(enteredNumber)) {
-        alert('Doh!');
-    } else {
+    if (stage === "start") {
+        var enteredNumber = parseInt($('#nrandrec').val(), 10);
+        if (isNaN(enteredNumber) || enteredNumber < 1 || enteredNumber > 100000) {
+            alert('Please enter a number between 1 and 100,000'); // alert('Doh!');
+            return;
+        } else {
+            nrand = enteredNumber;
+        }
         alert('simplerand(): got the number: ' + enteredNumber);
-        nrand = enteredNumber;
     }
     console.log(factors); //alert(factors); // doesn't print whole object
-    //var i, nrand, iform, seed, nranda, nrandb, trtdim, propx;
+    //var i, nrand, iform, seed, nranda, nrandb, treatment, propx;
     // fact1_1a,fact1_1b,fact1_2a,fact1_2b,fact2_1a, fact2_1b,fact2_2a,fact2_2b,fact2_3a,fact2_3b, fact3_1a,fact3_1b,fact3_2a,fact3_2b,fact3_3a, fact3_3b,fact3_4a,fact3_4b,factx_1a,factx_1b, factx_2a,factx_2b,fact1_1an,fact1_1bn,fact1_2an, fact1_2bn,fact2_1an,fact2_1bn,fact2_2an,fact2_2bn, fact2_3an,fact2_3bn,fact3_1an,fact3_1bn, fact3_2an,fact3_2bn,fact3_3an,fact3_3bn,fact3_4an, fact3_4bn,factx_1an,factx_1bn,factx_2an,factx_2bn
     // real ntoss, randno, propxpc
 
@@ -86,55 +98,55 @@ function simplerand() { // Takes in nrand, iform & propx?
     // Fortran version has individual variables for each element in each array in order to pass back to C program
     // initialise vars fact1_1a=0 fact1_1b=0 fact1_2a=0 fact1_2b=0 fact2_1a=0 fact2_1b=0 fact2_2a=0 fact2_2b=0 fact2_3a=0 fact2_3b=0 fact3_1a=0 fact3_1b=0 fact3_2a=0 fact3_2b=0 fact3_3a=0 fact3_3b=0 fact3_4a=0 fact3_4b=0 factx_1a=0 factx_1b=0 factx_2a=0 factx_2b=0 fact1_1an=0 fact1_1bn=0 fact1_2an=0 fact1_2bn=0 fact2_1an=0 fact2_1bn=0 fact2_2an=0 fact2_2bn=0 fact2_3an=0 fact2_3bn=0 fact3_1an=0 fact3_1bn=0 fact3_2an=0 fact3_2bn=0 fact3_3an=0 fact3_3bn=0 fact3_4an=0 fact3_4bn=0 factx_1an=0 factx_1bn=0 factx_2an=0 factx_2bn=0
     // nranda=0 nrandb=0 
-    var trtdim; // treatment dimension?
-    for (var i=1; i < nrand; i++) { // do i=1,nrand
+    var treatment; // treatment dimension?
+    for (var i = 1; i < nrand; i++) { // do i=1,nrand
         var randno = Math.random(); // flip a coin
         if (randno < 0.5) {     // treatment A
-            trtdim = 0;
+            treatment = 0;
             nranda++;// = nranda+1
         } else {                // treatment B
-            trtdim = 1;
+            treatment = 1;
             nrandb++; //=nrandb+1
-        }                    
+        }
 
         // simulate patient factors using random numbers instead of real data
 
         // factor 1 (duration of health problem): 0.7 long-term; 0.3 recent
-        randno = Math.random(); 
+        randno = Math.random();
         if (randno < 0.3) {
-            factors.fact1duration.s1longterm[trtdim]++; //fact1(1,trtdim)=fact1(1,trtdim)+1
+            factors.fact1duration.s1longterm[treatment]++; //fact1(1,treatment)=fact1(1,treatment)+1
         } else {
-            factors.fact1duration.s2recent[trtdim]++; //fact1(2,trtdim)=fact1(2,trtdim)+1
+            factors.fact1duration.s2recent[treatment]++; //fact1(2,treatment)=fact1(2,treatment)+1
         }
 
         // factor 2 (severity of health problem) 0.3: mild; 0.45 moderate; 0.25: severe
         randno = Math.random();
         if (randno < 0.3) {
-            factors.fact2severity.s1mild[trtdim]++; //fact2(1,trtdim)=fact2(1,trtdim)+1
+            factors.fact2severity.s1mild[treatment]++; //fact2(1,treatment)=fact2(1,treatment)+1
         } else if ((randno > 0.3) && (randno < 0.75)) {
-            factors.fact2severity.s2moderate[trtdim]++; //fact2(2,trtdim)=fact2(2,trtdim)+1
+            factors.fact2severity.s2moderate[treatment]++; //fact2(2,treatment)=fact2(2,treatment)+1
         } else {
-            factors.fact2severity.s3severe[trtdim]++; //fact2(3,trtdim)=fact2(3,trtdim)+1
+            factors.fact2severity.s3severe[treatment]++; //fact2(3,treatment)=fact2(3,treatment)+1
         }
 
         // factor 3 (age) 0.2= under 15; 0.25= 14-34 yrs; 0.25= 35-64 yrs; 0.3= 65 & older
         randno = Math.random();
         if (randno < 0.2) {
-            factors.fact3age.s1u15[trtdim]++; // fact3(1,trtdim)=fact3(1,trtdim)+1
+            factors.fact3age.s1u15[treatment]++; // fact3(1,treatment)=fact3(1,treatment)+1
         } else if ((randno > 0.2) && (randno < 0.45)) {
-            factors.fact3age.s2gt15lt35[trtdim]++; //fact3(2,trtdim)=fact3(2,trtdim)+1
+            factors.fact3age.s2gt15lt35[treatment]++; //fact3(2,treatment)=fact3(2,treatment)+1
         } else if ((randno > 0.45) && (randno < 0.7)) {
-            factors.fact3age.s3ge35lt65[trtdim]++; //fact3(3,trtdim)=fact3(3,trtdim)+1
+            factors.fact3age.s3ge35lt65[treatment]++; //fact3(3,treatment)=fact3(3,treatment)+1
         } else {
-            factors.fact3age.s4ge65[trtdim]++; //fact3(4,trtdim)=fact3(4,trtdim)+1
+            factors.fact3age.s4ge65[treatment]++; //fact3(4,treatment)=fact3(4,treatment)+1
         }
 
         // factor X (was a factor chosen by the user, now defaults to "Very anxious") (default 5%?)
         randno = Math.random();
         if (randno < 0.05) { // yes, default 5%
-            factors.fact4anxious.s1yes[trtdim]++; //factx(1,trtdim)=factx(1,trtdim)+1
+            factors.fact4anxious.s1yes[treatment]++; //factx(1,treatment)=factx(1,treatment)+1
         } else {
-            factors.fact4anxious.s2no[trtdim]++; //ffactx(2,trtdim)=factx(2,trtdim)+1
+            factors.fact4anxious.s2no[treatment]++; //ffactx(2,treatment)=factx(2,treatment)+1
         }
 
         // with variable factor, maybe not needed
@@ -143,9 +155,9 @@ function simplerand() { // Takes in nrand, iform & propx?
         // ... (as above)
         // } else {
         //     if (randno < propxpc) {
-        //         //factx(1,trtdim)=factx(1,trtdim)+1
+        //         //factx(1,treatment)=factx(1,treatment)+1
         //     } else {
-        //         factx(2,trtdim)=factx(2,trtdim)+1
+        //         factx(2,treatment)=factx(2,treatment)+1
         //     }
         // }
 
@@ -164,49 +176,49 @@ function toPercent(nrandx, factor, percentage) {
     percentage = Math.round(100 * factor / nrandx);
 }
 
-      if (nranda === 0) {
-        // fact1_1a=0
-      } else {
-      	// fact1_1a = nint(100.0*fact1(1,1)/real(nranda)) // 
-        //fact1_1a = Math.round(100 * fact1(1,1) / nranda)    
-      }
-      //fact1_1an=fact1(1,1)
+if (nranda === 0) {
+    // fact1_1a=0
+} else {
+    // fact1_1a = nint(100.0*fact1(1,1)/real(nranda)) // 
+    //fact1_1a = Math.round(100 * fact1(1,1) / nranda)    
+}
+//fact1_1an=fact1(1,1)
 
-      if (nrandb === 0) {
-        // fact1_1b=0
-      } else {
-     	// fact1_1b=nint(100.0*fact1(1,2)/real(nrandb))
-      }
-      // fact1_1bn=fact1(1,2)      
-      
-      if (nranda === 0) {
-        // fact1_2a=0
-      } else {
-        // fact1_2a=nint(100.0*fact1(2,1)/real(nranda))
-      }
-    //   fact1_2an=fact1(2,1)
-      
-      if (nrandb === 0) {
-        // fact1_2b=0
-      } else {
-        // fact1_2b=nint(100.0*fact1(2,2)/real(nrandb))
-      }
-    //   fact1_2bn=fact1(2,2)
-      
-      if (nranda === 0) {
-        // fact2_1a=0
-      } else {
-        // fact2_1a=nint(100.0*fact2(1,1)/real(nranda))
-      }
-    //   fact2_1an=fact2(1,1)
-      
-      if (nrandb === 0) {
-        // fact2_1b=0
-      } else {
-        // fact2_1b=nint(100.0*fact2(1,2)/real(nrandb))
-      }
-    //   fact2_1bn=fact2(1,2)
-      
+if (nrandb === 0) {
+    // fact1_1b=0
+} else {
+    // fact1_1b=nint(100.0*fact1(1,2)/real(nrandb))
+}
+// fact1_1bn=fact1(1,2)      
+
+if (nranda === 0) {
+    // fact1_2a=0
+} else {
+    // fact1_2a=nint(100.0*fact1(2,1)/real(nranda))
+}
+//   fact1_2an=fact1(2,1)
+
+if (nrandb === 0) {
+    // fact1_2b=0
+} else {
+    // fact1_2b=nint(100.0*fact1(2,2)/real(nrandb))
+}
+//   fact1_2bn=fact1(2,2)
+
+if (nranda === 0) {
+    // fact2_1a=0
+} else {
+    // fact2_1a=nint(100.0*fact2(1,1)/real(nranda))
+}
+//   fact2_1an=fact2(1,1)
+
+if (nrandb === 0) {
+    // fact2_1b=0
+} else {
+    // fact2_1b=nint(100.0*fact2(1,2)/real(nrandb))
+}
+//   fact2_1bn=fact2(1,2)
+
 //       if(nranda.eq.0)then
 //         fact2_2a=0
 //       else	
@@ -220,77 +232,77 @@ function toPercent(nrandx, factor, percentage) {
 //         fact2_2b=nint(100.0*fact2(2,2)/real(nrandb))
 //       endif
 //       fact2_2bn=fact2(2,2)
-      
+
 //       if(nranda.eq.0)then
 //         fact2_3a=0
 //       else	
 //         fact2_3a=nint(100.0*fact2(3,1)/real(nranda))
 //       endif
 //       fact2_3an=fact2(3,1)
-      
+
 //       if(nrandb.eq.0)then
 //         fact2_3b=0
 //       else	
 //         fact2_3b=nint(100.0*fact2(3,2)/real(nrandb))
 //       endif
 //       fact2_3bn=fact2(3,2)
-      
+
 //       if(nranda.eq.0)then
 //         fact3_1a=0
 //       else	
 //         fact3_1a=nint(100.0*fact3(1,1)/real(nranda))
 //       endif
 //       fact3_1an=fact3(1,1)	
-	
+
 //       if(nrandb.eq.0)then
 //         fact3_1b=0
 //       else	
 //         fact3_1b=nint(100.0*fact3(1,2)/real(nrandb))
 //       endif
 //       fact3_1bn=fact3(1,2)	
-	
+
 //       if(nranda.eq.0)then
 //         fact3_2a=0
 //       else	
 //         fact3_2a=nint(100.0*fact3(2,1)/real(nranda))
 //       endif
 //       fact3_2an=fact3(2,1)
-      
+
 //       if(nrandb.eq.0)then
 //         fact3_2b=0
 //       else	
 //         fact3_2b=nint(100.0*fact3(2,2)/real(nrandb))
 //       endif
 //       fact3_2bn=fact3(2,2)
-      
+
 //       if(nranda.eq.0)then
 //         fact3_3a=0
 //       else	
 //         fact3_3a=nint(100.0*fact3(3,1)/real(nranda))
 //       endif
 //       fact3_3an=fact3(3,1)
-      
+
 //       if(nrandb.eq.0)then
 //         fact3_3b=0
 //       else	
 //         fact3_3b=nint(100.0*fact3(3,2)/real(nrandb))
 //       endif
 //       fact3_3bn=fact3(3,2)
-      
+
 //       if(nranda.eq.0)then
 //         fact3_4a=0
 //       else	
 //         fact3_4a=nint(100.0*fact3(4,1)/real(nranda))
 //       endif
 //       fact3_4an=fact3(4,1)
-      
+
 //       if(nrandb.eq.0)then
 //         fact3_4b=0
 //       else	
 //         fact3_4b=nint(100.0*fact3(4,2)/real(nrandb))
 //       endif
 //       fact3_4bn=fact3(4,2)
-      	
+
 //       if(nranda.eq.0)then
 //         factx_1a=0
 //       else	
@@ -304,14 +316,14 @@ function toPercent(nrandx, factor, percentage) {
 //         factx_1b=nint(100.0*factx(1,2)/real(nrandb))
 //       endif
 //       factx_1bn=factx(1,2)
-      
+
 //       if(nranda.eq.0)then
 //         factx_2a=0
 //       else	
 //         factx_2a=nint(100.0*factx(2,1)/real(nranda))
 //       endif
 //       factx_2an=factx(2,1)
-      
+
 //       if(nrandb.eq.0)then
 //         factx_2b=0
 //       else	
