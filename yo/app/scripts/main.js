5cfd0a555b60debb2906f46f512c5d4713b5d1a3
Chris Bird <chrisjbird@gmail.com>
2017-02-14 22:45:13 +0000

    made Factors() function object

--- yo/app/scripts/main.js
+++ yo/app/scripts/main.js
@@ -1,7 +1,5 @@
 console.log('Ready...');
 
-var stage = "start", nrand = 0, nranda = 0, nrandb = 0, nrandNext = 0, treatANext = 0, treatBNext = 0;
-
 // gets NRAND random numbers between 0.0 & 1.0 for each of the 4 factor groups FACT1, FACT2, FACT3 & FACTX,
 // to determine the distributions between 2 theoretical treatment groups, treatment A & treatment B
 // Factor 1 : Duration of health problem    : 2 categories - long-term; recent
@@ -10,6 +8,8 @@ var stage = "start", nrand = 0, nranda = 0, nrandb = 0, nrandNext = 0, treatANex
 // Factor X : was a name factor with a percentage chosen by the user, now defaults to "Very anxious" (and what percentage?)
 // e.g. FACT3(2,1) is the number of patients for factor 2 (age), category 2 (15-34), on treatment A
 // possible gotchas: rand seed, 16-bit floating point rounding errors
+
+var stage = "start", nrand = 0, nranda = 0, nrandb = 0, nrandNext = 0, treatANext = 0, treatBNext = 0;
 
 // handle enter key on input
 $(function () {
@@ -36,7 +36,7 @@ function showPage(id) {
 // jQuery hide() and show() change "display", but there is no equivalent for "visibility"
 // so here is a jQuery plugin
 (function($) { // set visibility with visible() or invisible()
-    $.fn.invisible = function() {
+    $.fn.invisible = function() { // fn is an alias to prototype
         return this.each(function() {
             $(this).css("visibility", "hidden");
         });
@@ -61,6 +61,8 @@ function hideNext() {
     $("#summaryNext").hide(); // remove from layout as well
 }
 
+var base = new Factors();
+
 function begin() { // Takes in nrand, iform & propx?
     if (stage === "start") {
         var enteredNumber = parseInt($('#nrandrec').val(), 10);
@@ -70,9 +72,12 @@ function begin() { // Takes in nrand, iform & propx?
             return;
         } else {
             nrand = enteredNumber;
-            //base = new Factors();
-            simplerand(nrand); // , base);
-            fillBase(); // (base);
+            // var base = new Factors();
+            console.log('base: ' + base);
+            base.randomise(nrand);
+            console.log('base: ' + base);
+            //simplerand(nrand); // , base);
+            fillBase(base); // (base);
             hideNext(); // make sure "10 times bigger" values are hidden to begin with
             $('#intro').hide();     // intro "page"
             $('#results').show();   // results "page"
@@ -96,95 +101,172 @@ function makeBigger() {
 }
 
 //var Factors = function() { // each strata for each factor has a tuple for a count of patients randomised to treatment A or B
-var factors = { // each strata for each factor has a tuple for a count of patients randomised to treatment A or B
-    f1: { // duration
+// var factors = { // each strata for each factor has a tuple for a count of patients randomised to treatment A or B
+//     f1: { // duration
+//         s1: [0, 0],
+//         s2: [0, 0]
+//     },
+//     f2: { // severity
+//         s1: [0, 0],
+//         s2: [0, 0],
+//         s3: [0, 0]
+//     },
+//     f3: { // age
+//         s1: [0, 0],
+//         s2: [0, 0],
+//         s3: [0, 0],
+//         s4: [0, 0]
+//     },
+//     f4: { // anxious
+//         s1: [0, 0],
+//         s2: [0, 0]
+//     }
+// };   
+
+function Factors() { // each strata for each factor has a tuple for a count of patients randomised to treatment A or B
+    this.f1 = { // duration
         s1: [0, 0],
         s2: [0, 0]
-    },
-    f2: { // severity
+    };
+    this.f2 =  { // severity
         s1: [0, 0],
         s2: [0, 0],
         s3: [0, 0]
-    },
-    f3: { // age
+    };
+    this.f3 = { // age
         s1: [0, 0],
         s2: [0, 0],
         s3: [0, 0],
         s4: [0, 0]
-    },
-    f4: { // anxious
+    };
+    this.f4 = { // anxious
         s1: [0, 0],
         s2: [0, 0]
-    }
-};   
+    };
+};
 
-    // clear factors?
-    // use new?
-
-function simplerand(nrand) { // , factors) {
-    // FORTRAN version used a 2d array of which which treatments were randomised into which factors - use a JS object instead
-
-    //var base = new Factors();
-    console.log(factors);
-    console.log('nrand: ' + nrand + ', nranda: ' + nranda);
+Factors.prototype.randomise = function(nrand) {
     for (var i=1; i < nrand; i++) {         // do i=1,nrand
         var treatment;                      // treatment dimension?
         var randno = Math.random();         // flip a coin
         if (randno < 0.5) {                 // treatment A
             treatment = 0;                  // is an index into an array, remember, so 0-indexed
-            window.nranda += 1;
+            nranda += 1;
         } else {                            // treatment B
             treatment = 1;
             nrandb += 1;
         }
 
         // simulate patient factors using random numbers instead of real data
-
         // factor 1 (duration of health problem): 0.7 long-term; 0.3 recent
         randno = Math.random();
         if (randno < 0.3) {
-            factors.f1.s1[treatment]++;     //fact1(1,treatment)=fact1(1,treatment)+1
+            this.f1.s1[treatment]++;     //fact1(1,treatment)=fact1(1,treatment)+1
         } else {
-            factors.f1.s2[treatment]++;     //fact1(2,treatment)=fact1(2,treatment)+1
+            this.f1.s2[treatment]++;     //fact1(2,treatment)=fact1(2,treatment)+1
         }
 
         // factor 2 (severity of health problem) 0.3: mild; 0.45 moderate; 0.25: severe
         randno = Math.random();
         if (randno < 0.3) {
-            factors.f2.s1[treatment]++;     //fact2(1,treatment)=fact2(1,treatment)+1
+            this.f2.s1[treatment]++;     //fact2(1,treatment)=fact2(1,treatment)+1
         } else if ((randno > 0.3) && (randno < 0.75)) {
-            factors.f2.s2[treatment]++;     //fact2(2,treatment)=fact2(2,treatment)+1
+            this.f2.s2[treatment]++;     //fact2(2,treatment)=fact2(2,treatment)+1
         } else {
-            factors.f2.s3[treatment]++;     //fact2(3,treatment)=fact2(3,treatment)+1
+            this.f2.s3[treatment]++;     //fact2(3,treatment)=fact2(3,treatment)+1
         }
 
         // factor 3 (age) 0.2= under 15; 0.25= 14-34 yrs; 0.25= 35-64 yrs; 0.3= 65 & older
         randno = Math.random();
         if (randno < 0.2) {
-            factors.f3.s1[treatment]++;     // fact3(1,treatment)=fact3(1,treatment)+1
+            this.f3.s1[treatment]++;     // fact3(1,treatment)=fact3(1,treatment)+1
         } else if ((randno > 0.2) && (randno < 0.45)) {
-            factors.f3.s2[treatment]++;     //fact3(2,treatment)=fact3(2,treatment)+1
+            this.f3.s2[treatment]++;     //fact3(2,treatment)=fact3(2,treatment)+1
         } else if ((randno > 0.45) && (randno < 0.7)) {
-            factors.f3.s3[treatment]++;     //fact3(3,treatment)=fact3(3,treatment)+1
+            this.f3.s3[treatment]++;     //fact3(3,treatment)=fact3(3,treatment)+1
         } else {
-            factors.f3.s4[treatment]++;     //fact3(4,treatment)=fact3(4,treatment)+1
+            this.f3.s4[treatment]++;     //fact3(4,treatment)=fact3(4,treatment)+1
         }
 
         // factor X (was a factor chosen by the user, now defaults to "Very anxious") (default 5%?)
         randno = Math.random();
         if (randno < 0.05) {                // yes, default 5%
-            factors.f4.s1[treatment]++;     //factx(1,treatment)=factx(1,treatment)+1
+            this.f4.s1[treatment]++;     //factx(1,treatment)=factx(1,treatment)+1
         } else {
-            factors.f4.s2[treatment]++;     //ffactx(2,treatment)=factx(2,treatment)+1
+            this.f4.s2[treatment]++;     //ffactx(2,treatment)=factx(2,treatment)+1
         }
-    } // end do
-    console.log(factors);
-    console.log('nrand: ' + nrand + ', nranda: ' + nranda);
-
-
-    // and display
-    fillBase();
+    } // end do    
 }
+
+//var factors = new Factors();
+
+// clear factors?
+// use new?
+
+// function simplerand(nrand) { // , factors) {
+//     // FORTRAN version used a 2d array of which which treatments were randomised into which factors - use a JS object instead
+
+//     //var base = new Factors();
+//     console.log(factors);
+//     console.log('nrand: ' + nrand + ', nranda: ' + nranda);
+//     for (var i=1; i < nrand; i++) {         // do i=1,nrand
+//         var treatment;                      // treatment dimension?
+//         var randno = Math.random();         // flip a coin
+//         if (randno < 0.5) {                 // treatment A
+//             treatment = 0;                  // is an index into an array, remember, so 0-indexed
+//             window.nranda += 1;
+//         } else {                            // treatment B
+//             treatment = 1;
+//             nrandb += 1;
+//         }
+
+//         // simulate patient factors using random numbers instead of real data
+
+//         // factor 1 (duration of health problem): 0.7 long-term; 0.3 recent
+//         randno = Math.random();
+//         if (randno < 0.3) {
+//             factors.f1.s1[treatment]++;     //fact1(1,treatment)=fact1(1,treatment)+1
+//         } else {
+//             factors.f1.s2[treatment]++;     //fact1(2,treatment)=fact1(2,treatment)+1
+//         }
+
+//         // factor 2 (severity of health problem) 0.3: mild; 0.45 moderate; 0.25: severe
+//         randno = Math.random();
+//         if (randno < 0.3) {
+//             factors.f2.s1[treatment]++;     //fact2(1,treatment)=fact2(1,treatment)+1
+//         } else if ((randno > 0.3) && (randno < 0.75)) {
+//             factors.f2.s2[treatment]++;     //fact2(2,treatment)=fact2(2,treatment)+1
+//         } else {
+//             factors.f2.s3[treatment]++;     //fact2(3,treatment)=fact2(3,treatment)+1
+//         }
+
+//         // factor 3 (age) 0.2= under 15; 0.25= 14-34 yrs; 0.25= 35-64 yrs; 0.3= 65 & older
+//         randno = Math.random();
+//         if (randno < 0.2) {
+//             factors.f3.s1[treatment]++;     // fact3(1,treatment)=fact3(1,treatment)+1
+//         } else if ((randno > 0.2) && (randno < 0.45)) {
+//             factors.f3.s2[treatment]++;     //fact3(2,treatment)=fact3(2,treatment)+1
+//         } else if ((randno > 0.45) && (randno < 0.7)) {
+//             factors.f3.s3[treatment]++;     //fact3(3,treatment)=fact3(3,treatment)+1
+//         } else {
+//             factors.f3.s4[treatment]++;     //fact3(4,treatment)=fact3(4,treatment)+1
+//         }
+
+//         // factor X (was a factor chosen by the user, now defaults to "Very anxious") (default 5%?)
+//         randno = Math.random();
+//         if (randno < 0.05) {                // yes, default 5%
+//             factors.f4.s1[treatment]++;     //factx(1,treatment)=factx(1,treatment)+1
+//         } else {
+//             factors.f4.s2[treatment]++;     //ffactx(2,treatment)=factx(2,treatment)+1
+//         }
+//     } // end do
+//     console.log(factors);
+//     console.log('nrand: ' + nrand + ', nranda: ' + nranda);
+
+
+//     // and display
+//     fillBase();
+// }
 
 // Fortran: "convert to percentages and assign array values to variables to be passed back to main program"
 // NINT(A) rounds its argument to the nearest whole number. 0.5 rounds up to 1 by convention? Nothing in GNU docs...
@@ -210,61 +292,58 @@ factors.f4.s2[treatment]
 */
 
 // var stage = "start", nrand, nranda, nrandb, nrandNext, treatANext, treatBNext;
-//var errorNrandRec = "<p><font color=\"red\">*** Number of patients must be between 0 and 10000 ***</font></p>\n";
 
 var a = 0; // dev
-
 function fillBase(factors) {
     $('#nrand').html(window.nrand);
     $('#treatA').html(window.nranda);
     $('#treatB').html(window.nrandb);
     $('#totalA').html(window.nranda);
     $('#totalB').html(window.nrandb);
-    $('#t1f1s1').html(window.factors.f1.s1[0]); // (factors.f1.s1[0]);
-    // $('#t1f1s1').html(++a); // (factors.f1.s1[0]);
-    $('#t1f1s1pc').html(toPercent());
-    $('#t2f1s1').html(++a);
-    $('#t2f1s1pc').html(toPercent());
-    $('#t1f1s2').html(++a);
-    $('#t1f1s2pc').html(toPercent());
-    $('#t2f1s2').html(++a);
-    $('#t2f1s2pc').html(toPercent());
-    $('#t1f2s1').html(++a);
-    $('#t1f2s1pc').html(toPercent());
-    $('#t2f2s1').html(++a);
-    $('#t2f2s1pc').html(toPercent());
-    $('#t1f2s2').html(++a);
-    $('#t1f2s2pc').html(toPercent());
-    $('#t2f2s2').html(++a);
-    $('#t2f2s2pc').html(toPercent());
-    $('#t1f2s3').html(++a);
-    $('#t1f2s3pc').html(toPercent());
-    $('#t2f2s3').html(++a);
-    $('#t2f2s3pc').html(toPercent());
-    $('#t1f3s1').html(++a);
-    $('#t1f3s1pc').html(toPercent());
-    $('#t2f3s1').html(++a);
-    $('#t2f3s1pc').html(toPercent());
-    $('#t1f3s2').html(++a);
-    $('#t1f3s2pc').html(toPercent());
-    $('#t2f3s2').html(++a);
-    $('#t2f3s2pc').html(toPercent());
-    $('#t1f3s3').html(++a);
-    $('#t1f3s3pc').html(toPercent());
-    $('#t2f3s3').html(++a);
-    $('#t2f3s3pc').html(toPercent());
-    $('#t1f3s4').html(++a);
-    $('#t1f3s4pc').html(toPercent());
-    $('#t2f3s4').html(++a);
-    $('#t2f3s4pc').html(toPercent());
-    $('#t1f4s1').html(++a);
-    $('#t1f4s1pc').html(toPercent());
-    $('#t2f4s1').html(++a);
-    $('#t2f4s1pc').html(toPercent());
-    $('#t1f4s2').html(++a);
-    $('#t1f4s2pc').html(toPercent());
-    $('#t2f4s2').html(++a);
-    $('#t2f4s2pc').html(toPercent());
+    $('#f1s1t0').html(factors.f1.s1[0]); // (factors.f1.s1[0]);
+    $('#f1s1t0pc').html(toPercent(factors.f1.s1[0]));
+    $('#f1s1t1').html(factors.f1.s1[1]);
+    $('#f1s1t1pc').html(toPercent(factors.f1.s1[1]));
+    $('#f1s2t0').html(factors.f1.s2[0]);
+    $('#f1s2t0pc').html(toPercent(factors.f1.s2[0]));
+    $('#f1s2t1').html(factors.f1.s2[1]);
+    $('#f1s2t1pc').html(toPercent(factors.f1.s2[1]));
+    $('#f2s1t0').html(factors.f2.s1[0]);
+    $('#f2s1t0pc').html(toPercent(factors.f2.s1[0]));
+    $('#f2s1t1').html(factors.f2.s1[1]);
+    $('#f2s1t1pc').html(toPercent(factors.f2.s1[1]));
+    $('#f2s2t0').html(factors.f2.s2[0]);
+    $('#f2s2t0pc').html(toPercent(factors.f2.s2[0]));
+    $('#f2s2t1').html(factors.f2.s2[1]);
+    $('#f2s2t1pc').html(toPercent(factors.f2.s2[1]));
+    $('#f2s3t0').html(factors.f2.s3[0]);
+    $('#f2s3t0pc').html(toPercent(factors.f2.s3[0]));
+    $('#f2s3t1').html(factors.f2.s3[1]);
+    $('#f2s3t1pc').html(toPercent(factors.f2.s3[1]));
+    $('#f3s1t0').html(factors.f3.s1[0]);
+    $('#f3s1t0pc').html(toPercent(factors.f3.s1[0]));
+    $('#f3s1t1').html(factors.f3.s1[1]);
+    $('#f3s1t1pc').html(toPercent(factors.f3.s1[1]));
+    $('#f3s2t0').html(factors.f3.s2[0]);
+    $('#f3s2t0pc').html(toPercent(factors.f3.s2[0]));
+    $('#f3s2t1').html(factors.f3.s2[1]);
+    $('#f3s2t1pc').html(toPercent(factors.f3.s2[1]));
+    $('#f3s3t0').html(factors.f3.s3[0]);
+    $('#f3s3t0pc').html(toPercent(factors.f3.s3[0]));
+    $('#f3s3t1').html(factors.f3.s3[1]);
+    $('#f3s3t1pc').html(toPercent(factors.f3.s3[1]));
+    $('#f3s4t0').html(factors.f3.s4[0]);
+    $('#f3s4t0pc').html(toPercent(factors.f3.s4[0]));
+    $('#f3s4t1').html(factors.f3.s4[1]);
+    $('#f3s4t1pc').html(toPercent(factors.f3.s4[1]));
+    $('#f4s1t0').html(factors.f4.s1[0]);
+    $('#f4s1t0pc').html(toPercent(factors.f4.s1[0]));
+    $('#f4s1t1').html(factors.f4.s1[1]);
+    $('#f4s1t1pc').html(toPercent(factors.f4.s1[1]));
+    $('#f4s2t0').html(factors.f4.s2[0]);
+    $('#f4s2t0pc').html(toPercent(factors.f4.s2[0]));
+    $('#f4s2t1').html(factors.f4.s2[1]);
+    $('#f4s2t1pc').html(toPercent(factors.f4.s2[1]));
 }
 
 function fillNext() {
